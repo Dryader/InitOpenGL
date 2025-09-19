7e41284690cf2cbee33f9e5984183d87e5228e78
Yuval <@>
2025-09-19 17:18:26 -0400

    lab2 task 2

--- OpenGL/GameController.cpp
+++ OpenGL/GameController.cpp
@@ -29,10 +29,42 @@ void GameController::RunGame()
     m_mesh = Mesh();
     m_mesh.Create(&m_shader);
 	
+    GLFWwindow* window = WindowController::GetInstance().GetWindow();
+    int currentView = 0;
+    int tabKeyState = GLFW_RELEASE;
+
     do
     {
+        if (glfwGetKey(window, GLFW_KEY_TAB) == GLFW_PRESS && tabKeyState == GLFW_RELEASE) {
+            currentView = (currentView + 1) % 4;
+        }
+        tabKeyState = glfwGetKey(window, GLFW_KEY_TAB);
+
+        glm::mat4 view;
+        glm::mat4 projection;
+
+        switch (currentView)
+        {
+        case 0: // Perspective view 1
+            projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+            view = glm::lookAt(glm::vec3(4, 3, 3), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+            break;
+        case 1: // Perspective view 2
+            projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+            view = glm::lookAt(glm::vec3(-4, -3, 3), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+            break;
+        case 2: // Orthographic view 1
+            projection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, 0.0f, 100.0f);
+            view = glm::lookAt(glm::vec3(4, 3, 3), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+            break;
+        case 3: // Orthographic view 2
+            projection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, 0.0f, 100.0f);
+            view = glm::lookAt(glm::vec3(-10, -3, 3), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+            break;
+        }
+
         glClear(GL_COLOR_BUFFER_BIT); // Clear the screen
-    	m_mesh.Render(m_camera.GetProjection()* m_camera.GetView());
+    	m_mesh.Render(projection * view);
         glfwSwapBuffers(WindowController::GetInstance().GetWindow()); // Swap the front and back buffers
         glfwPollEvents();
     }
